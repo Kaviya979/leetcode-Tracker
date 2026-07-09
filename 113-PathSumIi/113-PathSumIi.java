8dca3653c65c90b156ab7f32c5551b28e01dacfd
Kaviya979 <kkaviyakkaviya53@gmail.com>
2026-07-09 15:10:05 +0530

    Create 113-PathSumIi

---
+++ 113-PathSumIi/113-PathSumIi.java
@@ -0,0 +1,38 @@
+// Last updated: 09/07/2026, 15:10:04
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+        List<List<Integer>> ans = new ArrayList<>();
+        dfs(root, targetSum, new ArrayList<>(), ans);
+        return ans;
+    }
+
+    private void dfs(TreeNode node, int sum, List<Integer> path, List<List<Integer>> ans) {
+        if (node == null) return;
+
+        path.add(node.val);
+
+        if (node.left == null && node.right == null && sum == node.val) {
+            ans.add(new ArrayList<>(path));
+        } else {
+            dfs(node.left, sum - node.val, path, ans);
+            dfs(node.right, sum - node.val, path, ans);
+        }
+
+        path.remove(path.size() - 1); 
+    }
+}
