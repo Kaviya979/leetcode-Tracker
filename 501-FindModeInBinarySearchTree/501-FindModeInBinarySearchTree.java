c9ecf9c1bd4fc5049d190f1bf9d32e9c91484c00
Kaviya979 <kkaviyakkaviya53@gmail.com>
2026-07-09 15:07:54 +0530

    Create 501-FindModeInBinarySearchTree

---
+++ 501-FindModeInBinarySearchTree/501-FindModeInBinarySearchTree.java
@@ -0,0 +1,72 @@
+// Last updated: 09/07/2026, 15:07:52
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+
+    private int currentVal;
+    private int currentCount = 0;
+    private int maxCount = 0;
+    private List<Integer> modes = new ArrayList<>();
+
+    public int[] findMode(TreeNode root) {
+        inorder(root);
+
+        int[] result = new int[modes.size()];
+        for (int i = 0; i < modes.size(); i++) {
+            result[i] = modes.get(i);
+        }
+        return result;
+    }
+
+    private void inorder(TreeNode node) {
+        if (node == null) return;
+
+        inorder(node.left);
+
+        if (currentCount == 0 || node.val != currentVal) {
+            currentVal = node.val;
+            currentCount = 1;
+        } else {
+            currentCount++;
+        }
+
+        if (currentCount > maxCount) {
+            maxCount = currentCount;
+            modes.clear();
+            modes.add(currentVal);
+        } else if (currentCount == maxCount) {
+            modes.add(currentVal);
+        }
+
+        inorder(node.right);
+    }
+}
