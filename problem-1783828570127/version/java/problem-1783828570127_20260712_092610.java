a237c03db4777fef446d57180b8cc3f042859936
Kaviya979 <kkaviyakkaviya53@gmail.com>
2026-07-12 09:26:13 +0530

    Create

---
+++ problem-1783828570127/version/java/problem-1783828570127_20260712_092610.java
@@ -0,0 +1,14 @@
+// Last updated: 12/07/2026, 09:26:10
+1class Solution {
+2    public int secondsBetweenTimes(String startTime, String endTime) {
+3        return getSeconds(endTime) - getSeconds(startTime);
+4    }
+5    private int getSeconds(String time){
+6        String[] parts = time.split(":");
+7        int h = Integer.parseInt(parts[0]);
+8        int m = Integer.parseInt(parts[1]);
+9        int s = Integer.parseInt(parts[2]);
+10        return h * 3600 + m * 60 + s;
+11    }
+12}
+13
