f9508daff5b610e7070adbf6af7103de306748d8
Kaviya979 <kkaviyakkaviya53@gmail.com>
2026-07-19 08:21:55 +0530

    Create

---
+++ problem-1784429514133/version/java/problem-1784429514133_20260719_082154.java
@@ -0,0 +1,8 @@
+// Last updated: 19/07/2026, 08:21:54
+1class Solution {
+2    public boolean canReach(int[] start, int[] target) {
+3        int startColor = (start[0] + start[1]) % 2;
+4        int targetColor = (target[0] + target[1]) % 2;
+5        return startColor == targetColor;
+6    }
+7}
