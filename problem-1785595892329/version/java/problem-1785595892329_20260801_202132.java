fc8d2fbcd376ffae591a190d22918c94e5ad6351
Kaviya979 <kkaviyakkaviya53@gmail.com>
2026-08-01 20:21:32 +0530

    Create

---
+++ problem-1785595892329/version/java/problem-1785595892329_20260801_202132.java
@@ -0,0 +1,21 @@
+// Last updated: 01/08/2026, 20:21:32
+1class Solution {
+2    public int countValidPrefixes(String s) {
+3        int count0 = 0;
+4        int count1 = 0;
+5        int validPrefixes = 0;
+6        for(int i = 0; i < s.length();i++){
+7            if(s.charAt(i) == '0'){
+8                count0++;
+9            }else{
+10                count1++;
+11            }
+12            if(Math.abs(count0 - count1) <= 1){
+13                validPrefixes++;
+14               }
+15            }
+16            return validPrefixes;
+17        }
+18    }
+19
+20    
